b9ddc3c140d763060557f4dd7d0c0bd7900edf9f
dav2113534 <dpalacios03081991@gmail.com>
2017-03-07 21:22:24 -0700

    Changed the html to something simplier and started adding js functions

--- index.js
+++ index.js
@@ -1,72 +1,36 @@
 //Making this quiz run using only javascript (no jquery)
-var questions = {
-    1: "Who is the host of Comedy Bang Bang?",
-    2: "What was the original name of the podcast",
-    3: "Which guest has the most appearences on the podcast?",
-    4: "What did Scott Name his dog?",
-    5: "What year did comedy bang bang start?",
-    6: "What is the famous catch phrase of the show?",
-    7: "who voices Scotts dad",
-    8: "Scotts alter ego is?",
-    9: "Who is banned from the podcast?",
-    10: "What is the last feature of each podcast?",
-};
 
-var choice1 = {
-    1: "Jason Mantzoukas",
-    2: "Comedy Death Ray",
-    3: "Claudia ODoherty",
-    4: "Rocky",
-    5: "1999",
-    6: "Whats up hotdog",
-    7: "Seth Morris",
-    8: "Hot Saucerman",
-    9: "Rob Huebal",
-    10: "Plugs",
-};
+var quiz = [{
+    "questions": "Who is the host of Comedy Bang Bang?",
+    "choices": ["Jason Mantzoukas", "Paul F. thompkins", "Nick Kroll", "Scott Aukerman"],
+    "correct": "Scott Aukerman"
+}, {
+    "questions": "What was the original name of the podcast",
+    "choices": ["Comedy Death Ray", "Comedy Bang Bang", "Hello from the magic Tavern", "Nerdist"],
+    "correct": "Comedy Death Ray"
+}, {
+    "questions": "What year did Comedy Bang Bang start?",
+    "choices": ["1999", "2007", "2008", "2009"],
+    "correct": "2009"
+}, {
+    "questions": "What is the famous catch phrase of the show?",
+    "choices": ["Whats up Hotdog", "Dog Days of Auggie Doggie", "Heynongman", "A peanut in the rain"],
+}]
 
-var choice2 = {
-    1: "Paul F. Thompkins",
-    2: "Comedy Bang Bang",
-    3: "Paul F. Thompkins",
-    4: "Doggie",
-    5: "2008",
-    6: "Dog days of auggie doggie",
-    7: "Paul Scheer",
-    8: "Weirder Scott",
-    9: "Paul Rust",
-    10: "What am I thinking",
+//initial vars 
+var currentQuestion = 0,
+    score = 0,
+    askingQuestion = true;
+
+function askQ() {
+    var choices = quiz[currentQuestion].choices,
+        ChoicesHtml = "";
 }
 
-var choice3 = {
-    1: "Nick Kroll",
-    2: "Hello from the magic tavern",
-    3: "Lauren Lapkus",
-    4: "Freddy",
-    5: "2017",
-    6: "Heynongman",
-    7: "James Adomian",
-    8: "Homeofthe Whooperman",
-    9: "Brett Gelman",
-    10: "Rap Battle",
-};
-
-var choice4 = {
-    1: "Scott Aukerman",
-    2: "Nerdist",
-    3: "Andy Daly",
-    4: "Garfield",
-    5: "2009",
-    6: "A peanut in the rain",
-    7: "Horatio Sanz",
-    8: "Avoid Noiderman",
-    9: "Nathan Fielder",
-    10: "Would you rather",
-};
-
-var answers = {
-    1: 2,
-    2: 4,
-    3: 1,
-    4: 3,
-};
+// will loop through choices and create butons
+for (var i = 0; i < choices.length; i++) {
+    choicesHtml += "<input type='radio' name='quiz" + currentQuestion +
+        "' id='choice" + (i + 1) +
+        "' value='" + choices[i] + "'>" +
+        " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label><br>";
+}
