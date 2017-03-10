4e678c54dbb194b9e0a6864ae278c1f81a08c93a
dav2113534 <dpalacios03081991@gmail.com>
2017-03-10 08:51:51 -0700

    added checkAnswer function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Making this quiz run using only javascript (no jquery)
 // creating array of objects will make it easier instead of creating each individual vars 
+
+
 var quiz = [{
     "questions": "Who is the host of Comedy Bang Bang?",
     "choices": ["Jason Mantzoukas", "Paul F. thompkins", "Nick Kroll", "Scott Aukerman"],
@@ -18,105 +20,46 @@ var quiz = [{
     "correct": "Whats up Hotdog"
 }];
 
+var currentQuestion = 0
 
-// define elements
-var content = $("content"),
-    questionContainer = $("question"),
-    choicesContainer = $("choices"),
-    scoreContainer = $("score"),
-    submitBtn = $("submit");
 
-// init vars
-var currentQuestion = 0,
-    score = 0,
-    askingQuestion = true;
-
-function $(id) { // shortcut for document.getElementById
-    return document.getElementById(id);
-}
-
-function askQuestion() {
-    var choices = quiz[currentQuestion].choices,
-        choicesHtml = "";
-
-    // loop through choices, and create radio buttons
-    for (var i = 0; i < choices.length; i++) {
-        choicesHtml += "<input type='radio' name='quiz" + currentQuestion +
-            "' id='choice" + (i + 1) +
-            "' value='" + choices[i] + "'>" +
-            " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label><br>";
+function checkAnswer(choice, correct) {
+    for (var i = 0; i < quiz[0].length; i++);
+    if (quiz[0].choices === quiz[0].correct) {
+        return true;
+    } else {
+        return false;
     }
-
-    // load the question
-    questionContainer.textContent = "Q" + (currentQuestion + 1) + ". " +
-        quiz[currentQuestion].questions;
-
-    // load the choices
-    choicesContainer.innerHTML = choicesHtml;
-
-    // setup for the first time
-    if (currentQuestion === 0) {
-        scoreContainer.textContent = "Score: 0 right answers out of " +
-            quiz.length + " possible.";
-        submitBtn.textContent = "Submit Answer";
+    if (quiz[1].choices === quiz[1].correct) {
+        return true;
+    } else {
+        return false;
+    }
+    if (quiz[2].choices === quiz[2].correct) {
+        return true;
+    } else {
+        return false;
+    }
+    if (quiz[3].choices === quiz[3].correct) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-function checkAnswer() {
-    // checks the answer and the submit button changes to next 
-    if (askingQuestion) {
-        submitBtn.textContent = "Next Question";
-        askingQuestion = false;
-
-        // determine which radio button they clicked
-        var userpick,
-            correctIndex,
-            radios = document.getElementsByName("quiz" + currentQuestion);
-        for (var i = 0; i < radios.length; i++) {
-            if (radios[i].checked) { // if this radio button is checked
-                userpick = radios[i].value;
-            }
-
-            // get index of correct answer
-            if (radios[i].value == quiz[currentQuestion].correct) {
-                correctIndex = i;
-            }
-        }
-
-        // This makes it visual if the choice picked is right or wrong
-        var labelStyle = document.getElementsByTagName("label")[correctIndex].style;
-        labelStyle.fontWeight = "bold";
-        if (userpick == quiz[currentQuestion].correct) {
-            score++;
-            labelStyle.color = "green";
-        } else {
-            labelStyle.color = "red";
-        }
-
-        scoreContainer.textContent = "Score: " + score + " right answers out of " +
-            quiz.length + " possible.";
-    } else { // move to next question
-        // setting up so user can ask a question
-        askingQuestion = true;
-        // change button text back to "Submit Answer"
-        submitBtn.textContent = "Submit Answer";
-        // if we're not on last question, increase question number
-        if (currentQuestion < quiz.length - 1) {
-            currentQuestion++;
-            askQuestion();
-        } else {
-            showFinalResults();
-        }
+function nextQuestion(currentQuestion, quiz) {
+    if (currentQuestion === quiz.correct) {
+        return quiz.questions;
+    } else {
+        return "Wrong";
     }
 }
 
-//This little guy down here returns the score from the quiz 
-function showFinalResults() {
-    content.innerHTML = "<h2>You've complited the quiz!</h2>" +
-        "<h2>Check Out Your Results!:</h2>" +
-        "<h2>" + score + " out of " + quiz.length + " questions, " +
-        Math.round(score / quiz.length * 100) + "%<h2>";
+function showResults(correct, wrong) {
+    var wrong = "Wrong";
 }
 
-window.addEventListener("load", askQuestion, false);
-submitBtn.addEventListener("click", checkAnswer, false);
+
+// checkAnswer
+// nextQuestion
+// showResults
